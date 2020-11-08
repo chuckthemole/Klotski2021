9fed6b9aae0b5e01b33bc3c82866865155a4a442
chuckthemole <chuckthemole@gmail.com>
2020-11-08 11:43:15 -0800

    Fix magic numbers KlotskiBoard

--- src/com/cpt/klotski/KlotskiBoard.java
+++ src/com/cpt/klotski/KlotskiBoard.java
@@ -22,28 +22,37 @@ public class KlotskiBoard {
     private static final int HORIZONTAL_BLOCK = 8;
     private static final int BIG_SQUARE = 9;
     
-    private
+    private final int ROW_ONE = 0;
+    private final int ROW_TWO = 100;
+    private final int ROW_THREE = 200;
+    private final int ROW_FOUR = 300;
+    
+    private final int COLUMN_ONE = 0;
+    private final int COLUMN_TWO = 100;
+    private final int COLUMN_THREE = 200;
+    private final int COLUMN_FOUR = 300;
+    private final int COLUMN_FIVE = 400;
 
     KlotskiBoard() {
         blocks = new KlotskiBlock[NUMBER_OF_BLOCKS];
         
         //one by one blocks
-        blocks[SMALL_SQUARE_1] = new KlotskiBlock("Small Square", 300, 400);  
-        blocks[SMALL_SQUARE_2] = new KlotskiBlock("Small Square", 100, 300);
-        blocks[SMALL_SQUARE_3] = new KlotskiBlock("Small Square", 200, 300);
-        blocks[SMALL_SQUARE_4] = new KlotskiBlock("Small Square", 0, 400);
+        blocks[SMALL_SQUARE_1] = new KlotskiBlock("Small Square", ROW_FOUR, COLUMN_FIVE);  
+        blocks[SMALL_SQUARE_2] = new KlotskiBlock("Small Square", ROW_TWO, COLUMN_FOUR);
+        blocks[SMALL_SQUARE_3] = new KlotskiBlock("Small Square", ROW_THREE, COLUMN_FOUR);
+        blocks[SMALL_SQUARE_4] = new KlotskiBlock("Small Square", ROW_ONE, COLUMN_FIVE);
         
         //one by two blocks 
-        blocks[VERTICAL_BLOCK_1] = new KlotskiBlock("Vertical Rectangle", 0, 0);          
-        blocks[VERTICAL_BLOCK_2] = new KlotskiBlock("Vertical Rectangle", 0, 200);         
-        blocks[VERTICAL_BLOCK_3] = new KlotskiBlock("Vertical Rectangle", 300, 0);       
-        blocks[VERTICAL_BLOCK_4] = new KlotskiBlock("Vertical Rectangle", 300, 200);  
+        blocks[VERTICAL_BLOCK_1] = new KlotskiBlock("Vertical Rectangle", ROW_ONE, COLUMN_ONE);          
+        blocks[VERTICAL_BLOCK_2] = new KlotskiBlock("Vertical Rectangle", ROW_ONE, COLUMN_THREE);         
+        blocks[VERTICAL_BLOCK_3] = new KlotskiBlock("Vertical Rectangle", ROW_FOUR, COLUMN_ONE);       
+        blocks[VERTICAL_BLOCK_4] = new KlotskiBlock("Vertical Rectangle", ROW_FOUR, COLUMN_THREE);  
         
         //two by one block 
-        blocks[HORIZONTAL_BLOCK] = new KlotskiBlock("Horizontal Rectangle", 100, 200); 
+        blocks[HORIZONTAL_BLOCK] = new KlotskiBlock("Horizontal Rectangle", ROW_TWO, COLUMN_THREE); 
         
         //two by two block        
-        blocks[BIG_SQUARE] = new KlotskiBlock("Big Square", 100, 0); 
+        blocks[BIG_SQUARE] = new KlotskiBlock("Big Square", ROW_TWO, COLUMN_ONE); 
     }
     
     public KlotskiBlock[] getBlocks() {
