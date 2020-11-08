b12c3869454f10825196f08368826d7f5f174c19
chuckthemole <chuckthemole@gmail.com>
2020-11-08 11:28:40 -0800

    Fix magic numbers in BlockDrag

--- src/com/cpt/klotski/BlockDrag.java
+++ src/com/cpt/klotski/BlockDrag.java
@@ -1,10 +1,4 @@
 package com.cpt.klotski;
-/**
- * Program name:    MouseDrag.java
- * Discussion:      Final
- * Written by:      Charles T
- * Due Date:        
- */
 
 import java.awt.Point;
 import javafx.scene.shape.Rectangle;
@@ -16,66 +10,62 @@ import javafx.scene.Cursor;
  *
  */
 public class BlockDrag {
-	private KlotskiBlock block;
-   
+	final int X_AXIS_BORDER_MAX = 400;
+	final int Y_AXIS_BORDER_MAX = 500;
+	final int X_AXIS_BORDER_MIN = 0;
+	final int Y_AXIS_BORDER_MIN = 0;
+	final int X_AXIS_LEFT_COLUMN_MAX = 100;
+	final int Y_AXIS_TOP_ROW_MAX = 100;
+	final int ZERO = 0;
+
     /**
      * 
      * @param b is a block on the klotskiBoard
      * @param klotskiBoard is the current KlotskiBoard object
      */
     BlockDrag(KlotskiBlock b, KlotskiBoard klotskiBoard, boolean mouseActive) {
-    	block = b;
     	Rectangle currentBlock = b.getRec();
-    	
         b.getRec().setCursor(Cursor.HAND);
         
-    	/*
-    	 * Sets the current block to the mouse's position
-    	 * Allows player to drag the game objects
-    	 */
         b.getRec().setOnMouseDragged(e -> {     	
         	currentBlock.toFront();
-        	double blockXBorder = 400 - b.getRec().getWidth();
-        	double blockYBorder = 500 - b.getRec().getHeight();
+        	final double BLOCKS_X_MAX_POSITION = X_AXIS_BORDER_MAX - b.getRec().getWidth();
+        	final double BLOCKS_Y_MAX_POSITION = Y_AXIS_BORDER_MAX - b.getRec().getHeight();
+        	final double HALF_BLOCK_WIDTH = currentBlock.getWidth() / 2;
+        	final double HALF_BLOCK_HEIGHT = (currentBlock.getHeight() / 2);
         	
         	if (mouseActive) {       	
 	        	// Right and left border walls
-	        	if (currentBlock.getX() >= blockXBorder && e.getX() > blockXBorder) {
-	        		currentBlock.setX(blockXBorder);
+	        	if (currentBlock.getX() >= BLOCKS_X_MAX_POSITION && e.getX() > BLOCKS_X_MAX_POSITION) {
+	        		currentBlock.setX(BLOCKS_X_MAX_POSITION);
 	        	}
-	        	else if (currentBlock.getX() <= 0 && e.getX() < 100) {
-	        		currentBlock.setX(0);
+	        	else if (currentBlock.getX() <= X_AXIS_BORDER_MIN && e.getX() < X_AXIS_LEFT_COLUMN_MAX) {
+	        		currentBlock.setX(ZERO);
 	        	}
-	        	else 
-	        		currentBlock.setX(e.getX() - (currentBlock.getWidth() / 2));
+	        	else {
+	        		currentBlock.setX(e.getX() - HALF_BLOCK_WIDTH);
+	        	}
 	        	
 	        	// Top and bottom border walls
-	        	if (currentBlock.getY() >= blockYBorder && e.getY() > blockYBorder) {
-	        		currentBlock.setY(blockYBorder);
+	        	if (currentBlock.getY() >= BLOCKS_Y_MAX_POSITION && e.getY() > BLOCKS_Y_MAX_POSITION) {
+	        		currentBlock.setY(BLOCKS_Y_MAX_POSITION);
 	        	}
-	        	else if (currentBlock.getY() <= 0 && e.getY() < 100) {
-	        		currentBlock.setY(0);
+	        	else if (currentBlock.getY() <= Y_AXIS_BORDER_MIN && e.getY() < Y_AXIS_TOP_ROW_MAX) {
+	        		currentBlock.setY(ZERO);
 	        	}
-	        	else 
-	        		currentBlock.setY(e.getY() - (currentBlock.getHeight() / 2));  
+	        	else {
+	        		currentBlock.setY(e.getY() - HALF_BLOCK_HEIGHT);  
+	        	}
         	}
         	e.consume();
         });
         
-        /*
-         * When mouse is released on a game object, check to make sure the space is available.
-         */
         b.getRec().setOnMouseReleased(e -> {
-        	//Rectangle currentBlock = b.getRec();
         	double x = currentBlock.getX();
         	double y = currentBlock.getY();
         	
-        	klotskiBoard.setBlockPosition(block, new Point((int) x, (int) y));
+        	klotskiBoard.setBlockPosition(b, new Point((int) x, (int) y)); // check move is legal
         	currentBlock.toBack();
         });
-    }
-    
-    public KlotskiBlock getBlock() {
-    	return block;
     }
 }
