f40ae792ff18c1500503a0e52cc3737a56ef60f6
chuckthemole <chuckthemole@gmail.com>
2020-12-28 13:04:03 -0800

    Fix drop point in BlockDrag

--- src/com/cpt/klotski/Buttons.java
+++ src/com/cpt/klotski/Buttons.java
@@ -11,90 +11,90 @@ public class Buttons extends Klotski {
     HBox hbox;
     Button restart;
     Button solve;
-    Button quit; 
+    Button quit;
     Button undo;
     Button music;
 
-    Buttons() {     
-    	hbox = new HBox();
-    	restart = new Button("Restart");
-    	solve = new Button("Solve");
-        quit = new Button("Quit"); 
+    Buttons() {
+        hbox = new HBox();
+        restart = new Button("Restart");
+        solve = new Button("Solve");
+        quit = new Button("Quit");
         undo = new Button("Undo");
         music = new Button("Music");
-    	
+
         // Set up HBox style
         hbox.setSpacing(10);
         hbox.setPadding(new Insets(0, 20, 10, 20));
-        //hbox.getChildren().addAll(restart, solve, quit, undo);
+        // hbox.getChildren().addAll(restart, solve, quit, undo);
         hbox.getChildren().add(restart);
         hbox.getChildren().add(solve);
         hbox.getChildren().add(quit);
         hbox.getChildren().add(undo);
         hbox.getChildren().add(music);
 
-        hbox.setStyle("-fx-padding: 10;" + 
-                      "-fx-border-style: solid inside;" + 
-                      "-fx-border-width: 2;" +
-                      "-fx-border-insets: 5;" + 
-                      "-fx-border-radius: 5;" + 
-                      "-fx-border-color: blue;");
-       
+        hbox.setStyle("-fx-padding: 10;" +
+                "-fx-border-style: solid inside;" +
+                "-fx-border-width: 2;" +
+                "-fx-border-insets: 5;" +
+                "-fx-border-radius: 5;" +
+                "-fx-border-color: blue;");
+
         hbox.setLayoutX(50);
         hbox.setLayoutY(550);
-        
-        music.setOnAction(new EventHandler<ActionEvent>() { 
+
+        music.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event) {
-            	music();
+                music();
             }
         });
-        
-        quit.setOnAction(new EventHandler<ActionEvent>() { 
+
+        quit.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event) {
-            	quit();
+                quit();
             }
         });
-        
+
         solve.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event) {
-            	solve();
+                solve();
             }
         });
-        
+
         restart.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event) {
-            	restart();    
+                restart();
             }
         });
-        
+
         undo.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event) {
-            	undo();
+                undo();
             }
         });
     }
-    
+
     public HBox getHBox() {
         return hbox;
     }
-    
+
     public Button getRestart() {
         return restart;
     }
-    
+
     public Button getUndo() {
         return undo;
     }
-    
+
     public Button getSolve() {
         return solve;
     }
-    
+
     public Button getQuit() {
         return quit;
     }
--- src/com/cpt/klotski/PlayMusic.java
+++ src/com/cpt/klotski/PlayMusic.java
@@ -1,17 +1,17 @@
 package com.cpt.klotski;
 
-//import java.io.FileInputStream;
+// import java.io.FileInputStream;
 import java.io.File;
-import javafx.scene.media.Media;  
-import javafx.scene.media.MediaPlayer;  
-//import java.io.InputStream;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+// import java.io.InputStream;
 
-//import javax.sound.sampled.AudioSystem;
-//import javax.sound.sampled.Clip;
+// import javax.sound.sampled.AudioSystem;
+// import javax.sound.sampled.Clip;
 import javax.swing.JOptionPane;
 
-//import sun.audio.AudioPlayer;
-//import sun.audio.AudioStream;
+// import sun.audio.AudioPlayer;
+// import sun.audio.AudioStream;
 
 /**
  * Audio file formats: AIFF, AU and WAV
@@ -20,68 +20,65 @@ import javax.swing.JOptionPane;
  *
  */
 public class PlayMusic {
-	private static Media hit;
-	private static MediaPlayer mediaPlayer;
-	private static Media slide = initBlockSlide();
-	private static MediaPlayer mp = new MediaPlayer(slide);
-	private static boolean musicIsPlaying;
-	
-	public static void playMusic(String filePath) {
-		try {
-			musicIsPlaying = true;
-			hit = new Media(new File(filePath).toURI().toString());
-			mediaPlayer = new MediaPlayer(hit);
-			mediaPlayer.play();
-			System.out.println("Playing Music...");
-		}
-		catch(Exception e) {
-			JOptionPane.showMessageDialog(null, "Error playing music!");
-		}
-	}
-	
-	public static Media initBlockSlide() {
-		if (OperatingSystem.isMac() || OperatingSystem.isUnix()) {
-			return new Media(new File("Music//slide.MP3").toURI().toString());
-		}
-		if (OperatingSystem.isWindows()) {
-			return new Media(new File("Music\\slide.MP3").toURI().toString());
-			
-		}
-		else return null;
-	}
-	
-	public static void stop() {
-		try {
-			mediaPlayer.stop();
-			System.out.println("Music Stopped....");
-			musicIsPlaying = false;
-		}
-		catch(Exception e) {
-			System.out.println(e);
-		}	
-	}
-	
-	public static void play() {
-		try {
-			mediaPlayer.play();
-			System.out.println("Music Started....");
-			musicIsPlaying = true;
-		}
-		catch(Exception e) {
-			JOptionPane.showMessageDialog(null, "Can't Stop Music");
-		}
-	}
-	
-	public static void playSlide() {
-		System.out.println("Block Sliding...");
-		mp.play();
-	}
-	
-	public static void stopSlide() {
-		mp.stop();
-	}
-	
-	public static boolean getMusicIsPlaying() {
-		return musicIsPlaying;
-	}
+    private static Media hit;
+    private static MediaPlayer mediaPlayer;
+    private static Media slide = initBlockSlide();
+    private static MediaPlayer mp = new MediaPlayer(slide);
+    private static boolean musicIsPlaying;
+
+    public static void playMusic(String filePath) {
+        try {
+            musicIsPlaying = true;
+            hit = new Media(new File(filePath).toURI().toString());
+            mediaPlayer = new MediaPlayer(hit);
+            mediaPlayer.play();
+            System.out.println("Playing Music...");
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "Error playing music!");
+        }
+    }
+
+    public static Media initBlockSlide() {
+        if (OperatingSystem.isMac() || OperatingSystem.isUnix()) {
+            return new Media(new File("Music//slide.MP3").toURI().toString());
+        }
+        if (OperatingSystem.isWindows()) {
+            return new Media(new File("Music\\slide.MP3").toURI().toString());
+
+        } else
+            return null;
+    }
+
+    public static void stop() {
+        try {
+            mediaPlayer.stop();
+            System.out.println("Music Stopped....");
+            musicIsPlaying = false;
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
+    public static void play() {
+        try {
+            mediaPlayer.play();
+            System.out.println("Music Started....");
+            musicIsPlaying = true;
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "Can't Stop Music");
+        }
+    }
+
+    public static void playSlide() {
+        System.out.println("Block Sliding...");
+        mp.play();
+    }
+
+    public static void stopSlide() {
+        mp.stop();
+    }
+
+    public static boolean getMusicIsPlaying() {
+        return musicIsPlaying;
+    }
 }
